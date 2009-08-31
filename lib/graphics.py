58b5eccf25f0dea356330ba63d26ac350d1e9ecc
Thomas Lundgaard <thomaslundgaard@gmail.com>
2009-08-31 20:01:26 +0200

    first working blower man (hopefully :) )

--- lib/graphics.py
+++ lib/graphics.py
@@ -12,22 +12,38 @@ class Blower (GraphicsBase):
     STOP = 0
     LEFT = 1
     RIGHT = 2
+    imageWidth = 110
+    imageHeight = 85
+    blowerManButtom2Fix = 16
+    blowerGunLeft2Fix = 84
     def __init__ (self,game):
         GraphicsBase.__init__ (game)
-        self.location = pygame.Rect (0,0,0,0);
-        self.location.center(self.game.res.cfg.screenWidth/2, self.game.res.cfg.schreenHeight-10)
+        self.location = pygame.Rect (0,0,imageWidth,imageHeight);
+        self.location.midbottom = (self.game.res.cfg.screenWidth/2, self.game.res.cfg.schreenHeight)
+        self.angle = 0
+        self.image = pygame.surface (imageWidth, imageHeight)
     def think (self, time):
         if (self.direction == LEFT):
             self.location.move_ip ( -self.game.res.cfg.blowerVelocity * time, 0)
         elif (self.direction == RIGHT):
             self.location.move_ip ( self.game.res.cfg.blowerVelocity * time, 0)
         
-        # If mouse is within window, calculate new angle
-        (mousePosX, mousePosY) = pygame.mouse.get_pos ()
-        if ( mousePosX > 0 and mousePosX < self.game.res.cfg.screenWidth and \
-                mousePosY > 0 and mousePosY < self.game.res.cfg.screenHeight ):
-            pass
+        if (self.location.left < 0):
+            self.location.left = 0
+        if (self.location.right > self.game.res.cfg.screenWidth):
+            self.location.right = self.game.res.cfg.screenWidth
 
+        # If mouse is within window, calculate new angle (else use old angle)
+        (mouseX, mouseY) = pygame.mouse.get_pos ()
+        if ( mouseX > 0 and mouseX < self.game.res.cfg.screenWidth and \
+                mouseY > 0 and mouseY < self.game.res.cfg.screenHeight ):
+            self.angle = atan2 ( self.game.res.cfg.screenHeight-blowerManButtom2Fix-mouseY, (-1)*(self.game.res.cfg.screenWidth-mouseX-imageWidth/2) )
+
+        # draw the blower
+        self.image.fill ((0,0,0,0))
+        self.image.blit (self.res.blowerMan, ( (imageWidth-self.res.blowerMan.get_width ())/2, (imageHeight-self.res.blowerMan.get_height()) ))
+        
+        
 class Air (GraphicsBase):
     def __init__ (self, game, location):
         GraphicsBase.__init__ (game)
