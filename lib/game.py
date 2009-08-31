515b64ce8b6317ac621e9b66ef1fe433e72537ed
Peter Bjørn Jørgensen <peter@archpad.(none)>
2009-08-31 15:54:12 +0200

    game.py updated

--- lib/game.py
+++ lib/game.py
@@ -4,15 +4,35 @@ import pygame
 
 class Game:
     def __init__(self, level, resources):
-        self.quit = false
+        self.quit = False
         self.level = level
         self.res = resources
         self.enemies = []
+        self.effects = []
+        self.feather = None #TODO
         self.blower = None #TODO
         self.clock = pygame.time.Clock()
     def run(self):
         while not self.quit:
-            pass
-     
+            frametime = float(self.clock.tick(90)) #maxfps
+            
+            self.res.screen.blit(self.res.background, (0,0)) #Clear screen
 
-        
+            #update game objects
+            self.blower.think(frametime)
+            self.feather.think(frametime)
+            for enemy in self.enemies:
+                enemy.think(frametime)
+            #delete dead enemies
+            self.enemies = [enemy for enemy in self.enemies if not enemy.dead]
+
+            for effect in self.effects:
+                effect.think(frametime)
+            self.effects = [effect for effect in self.effects if not effect.dead]
+            
+
+
+
+
+            
+
